4e665fecf564ef9cc95af14ec9587778c87607ba
Unknown <robertedwardsmail@gmail.com>
2017-07-26 10:52:33 +0200

    Added params and return tags

--- ablduck/dingus.js
+++ ablduck/dingus.js
@@ -5,15 +5,16 @@ var writer = new commonmark.HtmlRenderer({ sourcepos: true });
 var htmlwriter = new commonmark.HtmlRenderer({ sourcepos: false });
 var xmlwriter = new commonmark.XmlRenderer({ sourcepos: false });
 var reader = new commonmark.Parser();
-var isInternal = false;
-var deprecated = null;
 
-var tagTemplate = "( *?[TOKEN]\\s*?([\\s\\S]*?))(?:\\n\\s*?@author|\\n\\s*?@internal|\\n\\s*?@deprecated|\\n\\s*?@param|\\n\\s*?@return|$)";
+var tagTemplate = "( *?[TOKEN]\\s*?([\\s\\S]*?))(?=\\n\\s*?@author|\\n\\s*?@internal|\\n\\s*?@deprecated|\\n\\s*?@param|\\n\\s*?@return|$)";
 var tags        = ["@author", "@internal", "@deprecated", "@param", "@return"];
 var tagRegex    = {};
 
 var linkTag = new RegExp("{@link +(\\S+) *([\\s\\S]*?)}", "g");
 var deprecatedTag = new RegExp("@deprecated\\s+?(\\S+?)\\s+?([\\s\\S]*)");
+var paramTag = new RegExp("@param\\s+?(\\S+?)\\s+?([\\s\\S]*)");
+var returnTag = new RegExp("@return\\s+?([\\s\\S]*)");
+var authorTag = new RegExp("@author\\s+?([\\s\\S]*)");
 
 var commentLeadingAstrix = new RegExp("^\\s*\\*(?: |)(\\n?|[\\s\\S]+?)", "mg");
 var commentTagsAdd = new RegExp("^", "mg");
@@ -32,17 +33,19 @@ function getQueryVariable(variable) {
 
 var trimCommentLines = function(comment) {
 
+    var parsed = reader.parse(comment);
+
     var start = comment.indexOf('\n'),
         end   = comment.lastIndexOf('\n');
-
+    
     if (start === end)
         return "";
-
+    
     if (start != -1 && end != -1)
         comment = comment.substring(start + 1, end);
-
-    comment = comment.replace(commentLeadingAstrix, "$1");
-
+    
+    comment = comment.replace(commentLeadingAstrix, "$1");   
+    
     return comment;
 }
 
@@ -59,37 +62,39 @@ var render = function(parsed) {
     if (parsed === undefined) {
         return;
     }
-    var startTime = new Date().getTime();
+    
     var result = writer.render(parsed);
-    var endTime = new Date().getTime();
-    var renderTime = endTime - startTime;
-    var preview = $("#preview iframe").contents().find('.doc-contents');
 
-    if (isInternal)
-        result = '<div class="rounded-box private-box"><p><strong>NOTE:</strong>This is a private [method,property,class,constructor,event] for internal use by the framework. Don\'t rely on its existence.</p></div>' + result;
-    if (deprecated)
-        result = '<div class="rounded-box deprecated-box deprecated-tag-box"><p>This [method,property,class,constructor,event] has been <strong>deprected</strong> since [VERSION]</p><p>[MESSAGE]</p></div><br>'.replace("[VERSION]", deprecated[0]).replace("[MESSAGE]", deprecated[1]) + result;
-
-
-    preview.get(0).innerHTML = result;
-    $("#rendertime").text(renderTime);
+    return result;
 };
 
-var parseAndRender = function() {
+var parseAndRender = function(comment) {
+
+    //Parse the links out
+    comment = comment.replace(linkTag, function(complete_match, cls, link_text) { 
+        return "<a href='#'>" + (link_text || cls) + "</a>"; 
+    }); 
+
+    var parsed = reader.parse(comment);
+    
+    return render(parsed);
+};
+
+var parseWholeComment = function() {
     var textarea = $("#text");
-    var startTime = new Date().getTime();
+    var preview = $("#preview iframe").contents().find('.doc-contents');
 
     if ($("#commentTags").prop('checked')) 
         var comment = trimCommentLines(textarea.val());
-    else 
+    else
         var comment = textarea.val();
 
-    comment = comment.replace(linkTag, function(complete_match, cls, link_text) {
-        return "<a href='#'>" + (link_text || cls) + "</a>";
-    });
-
-    isInternal = false;
-    deprecated = null;
+    //Remove tags
+    var isInternal = false;
+    var deprecated = null;
+    var params = "";
+    var returns = "";
+    var author = "";
 
     for (var i = tags.length - 1; i >= 0; i--) {
         var match = comment.match(tagRegex[tags[i]]);
@@ -102,20 +107,55 @@ var parseAndRender = function() {
                     var dep = match[0].match(deprecatedTag);
                     deprecated = [];
                     deprecated.push(dep[1]);
-                    deprecated.push(dep[2]);
+                    deprecated.push(parseAndRender(dep[2]));
+                    break;
+                case '@param':
+                    params = "\n\n#### Parameters \n\n";
+                    comment = comment.replace(tagRegex[tags[i]], function(complete_match) {
+                        var p = complete_match.match(paramTag);
+                        params = params + " * <span class=\"pre\">" + p[1] + "</span> : DATATYPE  " + parseAndRender(p[2]);
+                        return "";
+                    });
+                    break;
+                case '@return':
+                    returns = "\n\n#### Returns \n\n";
+                    comment = comment.replace(tagRegex[tags[i]], function(complete_match) {
+                        var r = complete_match.match(returnTag);
+                        returns = returns + " * DATATYPE  " + parseAndRender(r[1]);
+                        return "";
+                    });
+                    break;
+                case '@author':
+                    author = "\n\n#### Author \n\n";
+                    var auth = match[0].match(authorTag);
+                    author = author + auth[1];
                     break;
             }
+            if (tags[i] != "@param" && tags[i] != "@return")
+                comment = comment.replace(tagRegex[tags[i]], "");   
         }
-        comment = comment.replace(tagRegex[tags[i]], "");   
+        
     }
 
-    var parsed = reader.parse(comment);
-    var endTime = new Date().getTime();
-    var parseTime = endTime - startTime;
-    $("#parsetime").text(parseTime);
-    $(".timing").css('visibility', 'visible');
-    render(parsed);
-};
+    if (params != "")
+        comment = comment + author;
+
+    if (params != "")
+        comment = comment + params;
+
+    if (returns != "")
+        comment = comment + returns;
+
+    var result = parseAndRender(comment);
+
+    if (isInternal)
+        result = '<div class="rounded-box private-box"><p><strong>NOTE:</strong>This is a private [method,property,class,constructor,event] for internal use by the framework. Don\'t rely on its existence.</p></div>' + result;
+    if (deprecated)
+        result = '<div class="rounded-box deprecated-box deprecated-tag-box"><p>This [method,property,class,constructor,event] has been <strong>deprected</strong> since [VERSION]</p><p>[MESSAGE]</p></div><br>'.replace("[VERSION]", deprecated[0]).replace("[MESSAGE]", deprecated[1]) + result;
+
+
+    preview.get(0).innerHTML = result;
+}
 
 $(document).ready(function() {
   $('iframe').on('load', function() {
@@ -127,29 +167,25 @@ $(document).ready(function() {
     }
 
     for (var i = tags.length - 1; i >= 0; i--) {
-        
         tagRegex[tags[i]] = new RegExp(tagTemplate.replace("[TOKEN]",  tags[i]), "g");
-   
     }
 
-    parseAndRender();
+    parseWholeComment();
 
     $("#clear-text-box").click(function() {
         textarea.val('');
-        parseAndRender();
+        parseWholeComment();
     });
 
     $("#indent").change(function() {
         if ($("#commentTags").prop('checked')) {
-            var textarea = $("#text");
             textarea.val(trimCommentLines(textarea.val()));
             textarea.val(addCommentLines(textarea.val()));
-            parseAndRender();
+            parseWholeComment();
         }
     });
 
     $("#commentTags").click(function() {
-        var textarea = $("#text");
         var commentTags = $("#commentTags").prop('checked');
         if (commentTags) {
             textarea.val(addCommentLines(textarea.val()));
@@ -157,13 +193,12 @@ $(document).ready(function() {
             textarea.val(trimCommentLines(textarea.val()));
         }
 
-        parseAndRender();
+        parseWholeComment();
 
     });
 
     textarea.bind('input propertychange',
-                  _.debounce(parseAndRender, 50, { maxWait: 100 }));
+                  _.debounce(parseWholeComment, 50, { maxWait: 100 }));
     
-
   });
 });
